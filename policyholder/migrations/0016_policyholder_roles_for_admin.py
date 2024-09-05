6e6454584aade8de9a5ccf97e141db65467a5908
delcroip <patrick.delcroix@swisstph.ch>
2024-09-05 22:47:21 +0200

    fix migration

--- policyholder/migrations/0016_policyholder_roles_for_admin.py
+++ policyholder/migrations/0016_policyholder_roles_for_admin.py
@@ -8,28 +8,28 @@ logger = logging.getLogger(__name__)
 
 
 def add_rights(apps, schema_editor):
-    insert_role_right_for_system(64, 150101)  # search
-    insert_role_right_for_system(64, 150102)  # create
-    insert_role_right_for_system(64, 150103)  # update
-    insert_role_right_for_system(64, 150104)  # delete
+    insert_role_right_for_system(64, 150101, apps)  # search
+    insert_role_right_for_system(64, 150102, apps)  # create
+    insert_role_right_for_system(64, 150103, apps)  # update
+    insert_role_right_for_system(64, 150104, apps)  # delete
 
-    insert_role_right_for_system(64, 150201)  # search
-    insert_role_right_for_system(64, 150202)  # create
-    insert_role_right_for_system(64, 150203)  # update
-    insert_role_right_for_system(64, 150204)  # delete
-    insert_role_right_for_system(64, 150206)  # refund
+    insert_role_right_for_system(64, 150201, apps)  # search
+    insert_role_right_for_system(64, 150202, apps)  # create
+    insert_role_right_for_system(64, 150203, apps)  # update
+    insert_role_right_for_system(64, 150204, apps)  # delete
+    insert_role_right_for_system(64, 150206, apps)  # refund
 
-    insert_role_right_for_system(64, 150301)  # search
-    insert_role_right_for_system(64, 150302)  # create
-    insert_role_right_for_system(64, 150303)  # update
-    insert_role_right_for_system(64, 150304)  # delete
-    insert_role_right_for_system(64, 150306)  # message
+    insert_role_right_for_system(64, 150301, apps)  # search
+    insert_role_right_for_system(64, 150302, apps)  # create
+    insert_role_right_for_system(64, 150303, apps)  # update
+    insert_role_right_for_system(64, 150304, apps)  # delete
+    insert_role_right_for_system(64, 150306, apps)  # message
 
-    insert_role_right_for_system(64, 150401)  # search
-    insert_role_right_for_system(64, 150402)  # create
-    insert_role_right_for_system(64, 150403)  # update
-    insert_role_right_for_system(64, 150404)  # delete
-    insert_role_right_for_system(64, 150406)  # message
+    insert_role_right_for_system(64, 150401, apps)  # search
+    insert_role_right_for_system(64, 150402, apps)  # create
+    insert_role_right_for_system(64, 150403, apps)  # update
+    insert_role_right_for_system(64, 150404, apps)  # delete
+    insert_role_right_for_system(64, 150406, apps)  # message
 
 
 class Migration(migrations.Migration):
