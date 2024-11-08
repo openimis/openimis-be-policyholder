ce8737d47b3a45ce62436d14b338f2a03d4e0a85
delcroip <patrick.delcroix@swisstph.ch>
2024-11-08 13:01:35 +0100

    wait for mutation

--- policyholder/schema.py
+++ policyholder/schema.py
@@ -3,6 +3,7 @@ import graphene_django_optimizer as gql_optimizer
 
 from django.db.models import Q
 from core.schema import OrderedDjangoFilterConnectionField, signal_mutation_module_validate
+from core.service import wait_for_mutation
 from core.utils import append_validity_filter
 from location.services import get_ancestor_location_filter
 from policyholder.models import PolicyHolder, PolicyHolderInsuree, PolicyHolderUser, \
@@ -63,11 +64,8 @@ class Query(graphene.ObjectType):
         dateValidFrom__Gte=graphene.DateTime(),
         dateValidTo__Lte=graphene.DateTime(),
         applyDefaultValidityFilter=graphene.Boolean()
-    )
-    validate_policy_holder_code = graphene.Field(
-        graphene.Boolean,
-        policy_holder_code=graphene.String(required=True),
-        description="Checks that the specified policy holder code is unique."
+    )from core.service import wait_for_mutation
+specified policy holder code is unique."
     )
 
     def resolve_validate_policy_holder_code(self, info, **kwargs):
@@ -109,6 +107,7 @@ class Query(graphene.ObjectType):
 
         client_mutation_id = kwargs.pop("clientMutationId", None)
         if client_mutation_id:
+            wait_for_mutation(client_mutation_id)
             filters.append(Q(mutations__mutation__client_mutation_id=client_mutation_id))
 
         parent_location = kwargs.get('parent_location')
